f640a8868925b5c134f0bf1257152fb2f989f31e
RodrigöMoya <r.moya@duocuc.cl>
2026-05-12 12:53:46 -0400

    Delete frontend directory

--- frontend/android/app/src/main/kotlin/com/example/process_sa/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.process_sa
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend/lib/core/constants.dart
+++
@@ -1,3 +0,0 @@
-class AppConstants {
-  static const String baseUrl = 'http://10.0.2.2:8080/api';
-}
